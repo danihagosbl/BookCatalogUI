28fe928a2857feb74143180bb9cf39582a95d42f
Daniel Tesfagabr <Daniel.Tesfagabr@bl.uk>
2024-05-16 10:12:56 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using BookCatalogUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace BookCatalogUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            var result = await client.GetAsync("https://localhost:7280/api/Todos");
+            //var result = await client.GetAsync("https://task-backend.internal.ambitiousforest-f9b17e64.uksouth.azurecontainerapps.io/tasks");
+            var text = await result.Content.ReadAsStringAsync();
+            ViewBag.Todos = JsonConvert.DeserializeObject<List<ToDo>>(text);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BookCatalogUI.Models
+{
+    public class ToDo
+    {
+        public int ID { get; set; }
+        public string? Description { get; set; }
+
+        [DisplayName("Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
