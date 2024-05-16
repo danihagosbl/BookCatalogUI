e667d5756c24c361c687a0b28c43bf8f8a674709
Daniel Tesfagabr <Daniel.Tesfagabr@bl.uk>
2024-05-16 10:40:36 +0100

    WIP

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace BookCatalogUI.Controllers
         public async Task<IActionResult> Index()
         {
             HttpClient client = new HttpClient();
-            var result = await client.GetAsync("https://localhost:7280/api/Todos");
-            //var result = await client.GetAsync("https://booksapi.whitedesert-a14a6cd8.uksouth.azurecontainerapps.io/api/Todos");
+            //var result = await client.GetAsync("https://localhost:7280/api/Todos");
+            var result = await client.GetAsync("https://booksapi.whitedesert-a14a6cd8.uksouth.azurecontainerapps.io/api/Todos");
             var text = await result.Content.ReadAsStringAsync();
             ViewBag.Todos = JsonConvert.DeserializeObject<List<ToDo>>(text);
             return View();
